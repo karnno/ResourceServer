646645ff21487be58333867737bbce36af5c1c19
karnno <karnno.noviseth@gmail.com>
2023-12-27 10:43:28 +0100

    Use @PreAuthorize and @PostAuthorize

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,8 +1,16 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.ws.api.ResourceServer.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
@@ -12,5 +20,21 @@ public class UsersController {
     public String status(){
         return "Working";
     }
-
+    
+    //		@PreAuthorize(value = "hasRole('developer')")    // PreAuthorize defines whether a method can be invoked or not. 
+	//      @PreAuthorize(value = "hasAuthority('ROLE_developer') or #id == #jwt.subject")  // Notice we use the prefix ROLE_
+    // 		@PostAuthorize(value = "")   	// PostAuthorize is called AFTER the method is called
+    //		@Secured("ROLE_developer") 		// the role as it appears in the JWT token
+    @PreAuthorize("#id == #jwt.subject")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+    	return "Delete user with id "+ id  + " and JWT subject " + jwt.getSubject();
+    }
+    
+    
+    @PostAuthorize("returnObject.userId == #jwt.subject")  // returnObject is a reserved term
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+    	return new UserRest("Karnno", "Nov", "686c79af-64b7-4dfd-99ba-9fc26f05096a");
+    }
 }
--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -1,36 +1,52 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
-@Configuration
+/**
+ * migrate from the deprecated WebSecurityConfigurerAdapter towards the content-based security configuration: 
+ * https://www.appsdeveloperblog.com/migrating-from-deprecated-websecurityconfigureradapter/
+ */
+// @org.springframework.context.annotation.Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // we can use the @Secured annotation, @PreAuthorized and @PostAuthorized
 @EnableWebSecurity
 public class WebSecurity {
 	
 	@Bean
 	DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
 	
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter()) ;
 		// allows us to define authorization rules as a parameter
 		http.authorizeHttpRequests( 
 				authz -> 
 					authz
-					.antMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")// extra filter on the user's profile
-					.anyRequest()
-					.authenticated() // ensures that URLs are allowed by any AUTHENTICATED user !
+					.antMatchers(HttpMethod.GET, "/users/status/check")
+					//hasAuthority("SCOPE_profile")	// extra filter on the user's profile
+					.hasRole   ("developer")  			
+					//.hasAnyRole("developer", "end_user")
+					//.hasAuthority("ROLE_developer") // notice the difference between hasAuthority that needs prefix SCOPE_ and hasRole that does not need a prefix
+					.anyRequest().authenticated() 				// ensures that URLs are allowed by any AUTHENTICATED user !
 				)
 			.oauth2ResourceServer( 					// setting our application to function as Oauth2 resource server
 				oauth2 ->
-					oauth2.jwt(jwt->{
-						
-					})								//and it means that our application will expect incoming requests 
-													//to have special kind of token known as JWT (Json Web Token), and it will
-													// be included as part of their security credentials
+					oauth2.jwt(jwt->{				//and it means that our application will expect incoming requests to have special kind of token known as JWT (Json Web Token), and it will be included as part of their security credentials
+						jwt.jwtAuthenticationConverter(converter); // we expect incoming requests to have a JWT and we set the converter that will read the roles.
+					})								
 				)
 		; 
+
+		// when filtering by roles, Spring boot must look INSIDE the access token. 
+		// Spring boot offers us to create a Converter Class that reads the list of roles and converts each role into a granted authority
+		
+		
+		
+		
 		
 		/* This is another way to write it.
 		http
